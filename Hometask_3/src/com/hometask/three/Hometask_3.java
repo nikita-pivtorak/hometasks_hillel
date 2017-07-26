6f246616c64435752a827360dfc78394f892e15f
ninikita-pivtorak <katapulter.inc@gmail.com>
2017-07-27 00:44:00 +0300

    Hometask 3 final

--- Hometask_3/src/com/hometask/three/Hometask_3.java
+++ Hometask_3/src/com/hometask/three/Hometask_3.java
@@ -1,6 +1,5 @@
-/**
- * Created by Nik on 26.07.2017.
- */
+package com.hometask.three;
+
 import java.util.Scanner;
 
 
@@ -17,13 +16,13 @@ public class Hometask_3 {
         System.out.println("Input deposit duration");
         int depositDuration = new Scanner(System.in).nextInt();
 
-        int percentAmount = 0 ;
-        int year = 0 ;
+        int percentAmount = 0;
+        int year = 0;
 
-        for (int i = 0 ; i < depositDuration  ; i++) {
+        for (int i = 0; i < depositDuration; i++) {
 
             percentAmount = (int) (moneyAmount * (percentByYear / 100));
-            moneyAmount =  (moneyAmount + percentAmount) ;
+            moneyAmount = (moneyAmount + percentAmount);
             year++;
 
             System.out.println("Money for " + year + " year(s) = " + moneyAmount +
