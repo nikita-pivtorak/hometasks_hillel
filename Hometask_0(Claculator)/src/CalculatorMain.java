d6782daff8d21b8d126d8b3317faed6d62a0db8a
ninikita-pivtorak <katapulter.inc@gmail.com>
2017-07-26 20:18:20 +0300

    Calculator version 2

--- Hometask_0(Claculator)/src/CalculatorMain.java
+++ Hometask_0(Claculator)/src/CalculatorMain.java
@@ -4,11 +4,17 @@
 
 import java.util.Scanner;
 
-public class Main {
+public class CalculatorMain {
 
         public static void main(String[] args) {
             System.out.println("Input number 1");
             int numberOne = new Scanner(System.in).nextInt();
+
+            while ( numberOne < -2147483648 || numberOne > 2147483647 ) {
+                System.out.println("The wrong value. Choose the muber from − 214748364...2147483647 gap");
+                numberOne = new Scanner(System.in).nextInt();
+
+            }
 
             System.out.println("Input number 2");
             int numberTwo = new Scanner(System.in).nextInt();
@@ -28,6 +34,10 @@ public class Main {
                     result = numberOne * numberTwo;
                     break;
                 case "/":
+                    while ( numberTwo == 0 ) {
+                        System.out.println("You cannot divide by zero. Choose another divider (Nonzero)" +
+                                " or input different operation type");
+                        numberTwo = new Scanner(System.in).nextInt();}
                     result = numberOne / numberTwo;
                     break;
                 case "%":
