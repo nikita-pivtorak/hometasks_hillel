562220db4cb3a865917e5556f2af23d252616300
ninikita-pivtorak <katapulter.inc@gmail.com>
2017-07-28 07:57:12 +0300

    Hometask 6 final

---
+++ Hometask_6/src/com/hometask/six/Hometsak_6.java
@@ -0,0 +1,35 @@
+package com.hometask.six;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Hometsak_6 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Input the number");
+        int number = new Scanner(System.in).nextInt();
+
+        int[] array = new int[number];
+        int modulo = 0;
+
+        for (int i = 1; i < number; i++) {
+
+            array[i] = i + 1;
+            modulo = modulo + (number % array[i]);
+            if (modulo == 0) {
+                System.out.println("It's a composite number");
+                break;
+            } else {
+                System.out.println("It's a prime number");
+                break;
+            }
+
+//            System.out.println(total);
+//            System.out.println(check);
+//            System.out.println(Arrays.toString(array));
+        }
+
+
+    }
+}
